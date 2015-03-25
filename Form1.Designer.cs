418827cb8cd4a6d9832e952edad7cc3b6d9a70b9
Rui <rshen26@uwo.ca>
2015-03-25 16:34:01 -0400

    SimpleSerial

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -54,7 +54,6 @@ namespace SimpleSerial
             this.label10 = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // buttonStart
@@ -278,22 +277,11 @@ namespace SimpleSerial
             this.label4.TabIndex = 24;
             this.label4.Text = "Comm Port";
             // 
-            // button1
-            // 
-            this.button1.Enabled = false;
-            this.button1.Location = new System.Drawing.Point(422, 230);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(99, 23);
-            this.button1.TabIndex = 25;
-            this.button1.Text = "Save Data";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(611, 477);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label11);
             this.Controls.Add(this.label10);
@@ -352,7 +340,6 @@ namespace SimpleSerial
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button1;
     }
 }
 
